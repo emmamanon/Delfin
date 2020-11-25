69a362b34c86215968ffe28955ade1e371c4a3dc
Raskoe99 <r.koefoed@hotmail.com>
2020-11-25 10:09:26 +0100

    .

--- src/DelfinAdmin/Træner.java
+++ src/DelfinAdmin/Træner.java
@@ -6,9 +6,4 @@ import java.util.ArrayList;
 public class Træner {
     private String name;
     private ArrayList<Medlem> hold;
-
-
-    public void sorterTop5() {
-        for (ArrayList<Medlem> )
-    }
 }
