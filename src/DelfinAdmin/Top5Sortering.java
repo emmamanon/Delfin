2a4a0fd7538e6976444e09e3d46319dc068f1bc8
Raskoe99 <r.koefoed@hotmail.com>
2020-11-25 13:26:12 +0100

    .

--- src/DelfinAdmin/Top5Sortering.java
+++ src/DelfinAdmin/Top5Sortering.java
@@ -27,7 +27,7 @@ public class Top5Sortering {
 
         for (KonkurrenceSvømmer konkurrenceSvømmer : topElever) {
             for (SvømmeDisciplin svømmeDisciplin : konkurrenceSvømmer.getSvømmediscipliner()) {
-                if(svømmeDisciplin.getNavn().equalsIgnoreCase("butterfly")) {  //Equalscaseignore?
+                if(svømmeDisciplin.getNavn().equalsIgnoreCase("butterfly")) {
                     butterfly.add(svømmeDisciplin);
                 } else if (svømmeDisciplin.getNavn().equalsIgnoreCase("crawl")) {
                     crawl.add(svømmeDisciplin);
@@ -38,17 +38,48 @@ public class Top5Sortering {
                 }
             }
         }
-        // Måske en ny metode?
-        void sortAfterPickUpTime() {
-            for (int i = 1; i < butterfly.size(); i++) {
-                for (int j = 0; j < butterfly.size(); j++) {
-                    Order temp = butterfly.get(j);
-                    if (temp.getPickUptime().isAfter(butterfly.get(i).getPickUptime())) {
-                        temp = butterfly.get(i);
-                        butterfly.set(i, butterfly.get(j));
-                        pizzaQueue.set(j, temp);
+        // Måske en ny metode?{
+        for (int i = 1; i < butterfly.size(); i++) {
+            for (int j = 0; j < butterfly.size(); j++) {
+                SvømmeDisciplin temp = butterfly.get(j);
+                if (temp.getTidISekunder() > (butterfly.get(i).getTidISekunder())) {
+                    temp = butterfly.get(i);
+                    butterfly.set(i, butterfly.get(j));
+                    butterfly.set(j, temp);
 
-                    }
+                }
+            }
+        }
+
+        for (int i = 1; i < crawl.size(); i++) {
+            for (int j = 0; j < crawl.size(); j++) {
+                SvømmeDisciplin temp = crawl.get(j);
+                if (temp.getTidISekunder() > (crawl.get(i).getTidISekunder())) {
+                    temp = crawl.get(i);
+                    crawl.set(i, crawl.get(j));
+                    crawl.set(j, temp);
+                }
+            }
+        }
+
+        for (int i = 1; i < rygCrawl.size(); i++) {
+            for (int j = 0; j < rygCrawl.size(); j++) {
+                SvømmeDisciplin temp = rygCrawl.get(j);
+                if (temp.getTidISekunder() > (rygCrawl.get(i).getTidISekunder())) {
+                    temp = rygCrawl.get(i);
+                    rygCrawl.set(i, rygCrawl.get(j));
+                    rygCrawl.set(j, temp);
+                }
+            }
+        }
+
+        for (int i = 1; i < brystsvømning.size(); i++) {
+            for (int j = 0; j < brystsvømning.size(); j++) {
+                SvømmeDisciplin temp = brystsvømning.get(j);
+                if (temp.getTidISekunder() > (brystsvømning.get(i).getTidISekunder())) {
+                    temp = brystsvømning.get(i);
+                    brystsvømning.set(i, brystsvømning.get(j));
+                    brystsvømning.set(j, temp);
                 }
             }
         }
